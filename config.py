d32e916565ed4937967186e11714e35947e4861d
limingyuan123 <1484156313@qq.com>
2019-12-12 22:26:00 +0800

    Initial commit

---
+++ config.py
@@ -0,0 +1,35 @@
+class DefaultCofig(object):
+
+    # log info
+    log_path = './logs/'
+
+    # model info
+    model = 'RefineNet'
+    batch_size = 2
+    use_gpu = True
+    epochs = 800
+    lr = 1e-6
+    momentum = 0.9
+    decay = 1e-4
+    original_lr = 1e-6
+    steps = [-1, 1, 100, 150]
+    scales = [1, 1, 1, 1]
+    workers = 4
+
+    # train&test files path
+    images = 'data/nyu_images'
+    labels = 'data/nyu_labels40'
+    depths = 'data/nyu_depths'
+    train_split = 'data/train.txt'
+    test_split = 'data/test.txt'
+
+    # saved model path
+    saved_model_path = 'saved_models/'
+
+    # test model file
+    test_model = saved_model_path + 'RefineNet_1220_20:38:52.pkl'
+
+    # predict files path
+    predict_images = 'data/predict/images'
+    predict_labels = 'data/predict/labels'
+    predict_depths = 'data/predict/depths'
